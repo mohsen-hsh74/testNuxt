7d4a5c3b1ba3c93367c56a0175f8ae2e0b755374
MohsenHassanshahi <a.hassanshahi74@gmail.com>
2022-11-28 11:31:13 +0330

    2

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://panjool.org',
+    baseURL: 'https://panjool.org',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
